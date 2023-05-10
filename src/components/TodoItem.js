89e586e7ac2a2c086545095c5965bbb68fd1ca4a
ponniepark <127806879+ponniepark@users.noreply.github.com>
2023-05-10 21:34:08 +0900

    commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,6 +11,7 @@ import styles from "@/styles/TodoList.module.css";
 const TodoItem = ({ todo, onToggle, onDelete }) => {
   // 각 할 일 항목을 렌더링합니다.
   return (
+
     <li className={styles.todoItem}>
       {/* 체크박스를 렌더링하고, 체크박스의 상태를 할 일의 완료 상태와 동기화합니다.
           체크박스의 상태가 변경되면 onToggle 함수를 호출하여 완료 상태를 업데이트합니다. */}
@@ -26,6 +27,9 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
       <button onClick={onDelete}>Delete</button>
+
+
+
     </li>
   );
 };
--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -61,6 +61,10 @@ export default function Signin() {
           >
             Sign in
           </button>
+
+
+
+          
         </div>
       )}
     </div>
